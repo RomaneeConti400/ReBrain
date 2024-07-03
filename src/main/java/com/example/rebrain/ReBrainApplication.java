8ff8e4b10043b63da8916c13dcdc58666300cf8f
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-04 01:03:04 +0300

    Initial commit

---
+++ src/main/java/com/example/rebrain/ReBrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReBrainApplication.class, args);
+    }
+
+}
