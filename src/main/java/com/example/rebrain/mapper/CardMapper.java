28ca8912006d394e7a3e703b1716bc446b30eed2
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-09 13:41:01 +0300

    feat: Add Mappers for converting between Dto and Entity

---
+++ src/main/java/com/example/rebrain/mapper/CardMapper.java
@@ -0,0 +1,29 @@
+package com.example.rebrain.mapper;
+
+import com.example.rebrain.dto.CardDto;
+import com.example.rebrain.entity.CardEntity;
+
+public class CardMapper {
+
+    public static CardEntity toEntity(CardDto cardDto) {
+        if (cardDto == null) {
+            return null;
+        }
+        CardEntity cardEntity = new CardEntity();
+        cardEntity.setId(cardDto.getId());
+        cardEntity.setTitle(cardDto.getTitle());
+        cardEntity.setDescr(cardDto.getDescr());
+        return cardEntity;
+    }
+
+    public static CardDto toDto(CardEntity cardEntity) {
+        if (cardEntity == null) {
+            return null;
+        }
+        CardDto cardDto = new CardDto();
+        cardDto.setId(cardEntity.getId());
+        cardDto.setTitle(cardEntity.getTitle());
+        cardDto.setDescr(cardEntity.getDescr());
+        return cardDto;
+    }
+}
