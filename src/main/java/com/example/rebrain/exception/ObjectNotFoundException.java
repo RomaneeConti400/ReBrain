5b68f4dfad7ff1a5fe31ad5460cb7ce8a76476d2
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-09 13:57:34 +0300

    feat: ObjectNotFoundException extends RuntimeException

--- src/main/java/com/example/rebrain/exception/ObjectNotFoundException.java
+++ src/main/java/com/example/rebrain/exception/ObjectNotFoundException.java
@@ -1,8 +1,7 @@
 package com.example.rebrain.exception;
 
-public class ObjectNotFoundException extends Exception {
+public class ObjectNotFoundException extends RuntimeException {
     public ObjectNotFoundException(String message) {
-
         super(message);
     }
 }
