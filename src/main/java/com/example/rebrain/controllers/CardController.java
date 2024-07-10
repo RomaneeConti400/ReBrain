09d6fd1652a498378977ec2f4150e32eacb47296
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-10 13:28:52 +0300

    feat: redesigned Card components to work with ControllerAdvice

--- src/main/java/com/example/rebrain/controllers/CardController.java
+++ src/main/java/com/example/rebrain/controllers/CardController.java
@@ -1,12 +1,10 @@
 package com.example.rebrain.controllers;
 
 import com.example.rebrain.dto.CardDto;
-import com.example.rebrain.dto.CardDto;
-import com.example.rebrain.dto.UpdateNoteDto;
 import com.example.rebrain.entity.CardEntity;
-import com.example.rebrain.exception.ObjectNotFoundException;
 import com.example.rebrain.mapper.CardMapper;
 import com.example.rebrain.services.CardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 @RequestMapping("/cards")
 public class CardController {
 
@@ -27,64 +26,38 @@ public class CardController {
 
     @PostMapping
     public ResponseEntity<CardDto> createCard(@RequestBody CardDto cardDto) {
-        try {
-            CardEntity cardEntity = CardMapper.toEntity(cardDto);
-            CardEntity createdCard = cardService.create(cardEntity);
-            CardDto createdToDto = CardMapper.toDto(createdCard);
-            URI location = URI.create("/cards/" + createdToDto.getId());
-            return ResponseEntity.created(location).body(createdToDto);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+        CardEntity cardEntity = CardMapper.toEntity(cardDto);
+        CardEntity createdCard = cardService.create(cardEntity);
+        CardDto createdToDto = CardMapper.toDto(createdCard);
+        URI location = URI.create("/cards/" + createdToDto.getId());
+        return ResponseEntity.created(location).body(createdToDto);
     }
 
     @GetMapping
     public ResponseEntity<List<CardDto>> getAllCards() {
-        try {
-            List<CardEntity> cardsEntities = cardService.getAll();
-            List<CardDto> cardDtos = cardsEntities.stream()
-                    .map(CardMapper::toDto)
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(cardDtos);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+        List<CardEntity> cardsEntities = cardService.getAll();
+        List<CardDto> cardDtos = cardsEntities.stream()
+                .map(CardMapper::toDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(cardDtos);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CardDto> getCardById(@PathVariable Integer id) {
-        try {
-            CardDto card = CardMapper.toDto(cardService.getOne(id));
-            return ResponseEntity.ok(card);
-        } catch (ObjectNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+        CardDto card = CardMapper.toDto(cardService.getOne(id));
+        return ResponseEntity.ok(card);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<CardDto> updateCard(@PathVariable Integer id, @RequestBody CardDto cardDto) {
-        try {
-            CardEntity updateEntity = CardMapper.toEntity(cardDto);
-            CardDto updatedCard = CardMapper.toDto(cardService.update(id, updateEntity));
-            return ResponseEntity.ok(updatedCard);
-        } catch (ObjectNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+        CardEntity updateEntity = CardMapper.toEntity(cardDto);
+        CardDto updatedCard = CardMapper.toDto(cardService.update(id, updateEntity));
+        return ResponseEntity.ok(updatedCard);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCard(@PathVariable Integer id) {
-        try {
-            cardService.delete(id);
-            return ResponseEntity.noContent().build();
-        } catch (ObjectNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(null);
-        }
+        cardService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
