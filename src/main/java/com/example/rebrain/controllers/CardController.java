b074c382059267e88af2c08a7c2ca309c94db22a
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-15 13:28:36 +0300

    feat: added logs

--- src/main/java/com/example/rebrain/controllers/CardController.java
+++ src/main/java/com/example/rebrain/controllers/CardController.java
@@ -5,7 +5,6 @@ import com.example.rebrain.entity.CardEntity;
 import com.example.rebrain.mapper.CardMapper;
 import com.example.rebrain.services.CardService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,38 +25,48 @@ public class CardController {
 
     @PostMapping
     public ResponseEntity<CardDto> createCard(@RequestBody CardDto cardDto) {
+        log.debug("Creating card with data: {}", cardDto);
         CardEntity cardEntity = CardMapper.toEntity(cardDto);
         CardEntity createdCard = cardService.create(cardEntity);
         CardDto createdToDto = CardMapper.toDto(createdCard);
         URI location = URI.create("/cards/" + createdToDto.getId());
+        log.info("Card created with ID: {}", createdToDto.getId());
         return ResponseEntity.created(location).body(createdToDto);
     }
 
     @GetMapping
     public ResponseEntity<List<CardDto>> getAllCards() {
+        log.debug("Fetching all cards");
         List<CardEntity> cardsEntities = cardService.getAll();
         List<CardDto> cardDtos = cardsEntities.stream()
                 .map(CardMapper::toDto)
                 .collect(Collectors.toList());
+        log.debug("Returning {} cards", cardDtos.size());
         return ResponseEntity.ok(cardDtos);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CardDto> getCardById(@PathVariable Integer id) {
+        log.debug("Fetching card with ID: {}", id);
         CardDto card = CardMapper.toDto(cardService.getOne(id));
+        log.debug("Card found: {}", card);
         return ResponseEntity.ok(card);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<CardDto> updateCard(@PathVariable Integer id, @RequestBody CardDto cardDto) {
+        log.debug("Updating card with ID: {} with data: {}", id, cardDto);
         CardEntity updateEntity = CardMapper.toEntity(cardDto);
         CardDto updatedCard = CardMapper.toDto(cardService.update(id, updateEntity));
+        log.info("Card with ID: {} updated", id);
         return ResponseEntity.ok(updatedCard);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCard(@PathVariable Integer id) {
+        log.info("Deleting card with ID: {}", id);
         cardService.delete(id);
+        log.info("Card with ID: {} deleted", id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/rebrain/services/CardService.java
+++ src/main/java/com/example/rebrain/services/CardService.java
@@ -17,19 +17,28 @@ public class CardService {
     private final CardRepo cardRepo;
 
     public CardEntity create(CardEntity cardEntity) {
-        log.info("Saving new {}", cardEntity);
-        return cardRepo.save(cardEntity);
+        log.debug("Saving new card: {}", cardEntity);
+        CardEntity savedEntity = cardRepo.save(cardEntity);
+        log.info("Card saved with ID: {}", savedEntity.getId());
+        return savedEntity;
     }
 
     public List<CardEntity> getAll() {
+        log.debug("Fetching all cards");
         return cardRepo.findAll();
     }
 
     public CardEntity getOne(Integer id) {
-        return cardRepo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Card with ID " + id + " not found"));
+        log.debug("Fetching card with ID: {}", id);
+        return cardRepo.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Card with ID: {} not found", id);
+                    return new ObjectNotFoundException("Card with ID " + id + " not found");
+                });
     }
 
     public CardEntity update(Integer id, CardEntity updateEntity) {
+        log.debug("Updating card with ID: {} with data: {}", id, updateEntity);
         CardEntity cardEntity = getEntityById(id);
         if (updateEntity.getTitle() != null) {
             cardEntity.setTitle(updateEntity.getTitle());
@@ -37,15 +46,24 @@ public class CardService {
         if (updateEntity.getDescription() != null) {
             cardEntity.setDescription(updateEntity.getDescription());
         }
-        return cardRepo.save(cardEntity);
+        CardEntity updatedEntity = cardRepo.save(cardEntity);
+        log.info("Card with ID: {} updated", id);
+        return updatedEntity;
     }
 
     public void delete(Integer id) {
+        log.debug("Deleting card with ID: {}", id);
         CardEntity cardEntity = getEntityById(id);
         cardRepo.delete(cardEntity);
+        log.info("Card with ID: {} deleted", id);
     }
 
     private CardEntity getEntityById(Integer id) {
-        return cardRepo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Card with ID " + id + " not found"));
+        log.debug("Fetching card entity with ID: {}", id);
+        return cardRepo.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Card entity with ID: {} not found", id);
+                    return new ObjectNotFoundException("Card with ID " + id + " not found");
+                });
     }
 }
