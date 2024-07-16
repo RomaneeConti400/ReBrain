6147a1694821b5f49c039eb8c3ec500675eaaa7e
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-16 14:31:02 +0300

    fix: V3 migration

--- src/main/resources/templates/db/migration/V3__Add_Users.sql
+++ src/main/resources/templates/db/migration/V3__Add_Users.sql
@@ -1,28 +1,3 @@
-create table cards (
-    id BIGINT not null,
-    description varchar(255),
-    title varchar(255),
-    user_id varchar(255),
-    primary key (id)
-);
-
-create table notes (
-    id BIGINT not null,
-    description varchar(255),
-    text varchar(255),
-    title varchar(255),
-    user_id varchar(255),
-    primary key (id)
-);
-
-create table sets (
-    id BIGINT not null,
-    description varchar(255),
-    title varchar(255),
-    user_id varchar(255),
-    primary key (id)
-);
-
 create table users (
     id BIGINT not null,
     first_name varchar(255),
