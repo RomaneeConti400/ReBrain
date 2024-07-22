16db5c547238ea87581b9aeb8f09d181dc088371
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-07-22 14:23:02 +0300

    refactor: removed unnecessary fields in answer entity

--- src/main/java/com/example/rebrain/entity/Answer.java
+++ src/main/java/com/example/rebrain/entity/Answer.java
@@ -5,14 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Table(name = "cards")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Answer {
-    @Column(name = "title")
     private Integer cardId;
-
-    @Column(name = "description")
     private String answer;
 }
