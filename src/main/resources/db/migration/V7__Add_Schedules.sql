a5d0b4bac37420debb592ace4cb054a1ab93d623
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-08-19 12:53:08 +0300

    feat: added schedule migration

---
+++ src/main/resources/db/migration/V7__Add_Schedules.sql
@@ -0,0 +1,7 @@
+create table if not exists schedules (
+    id BIGINT not null,
+    set_id BIGINT not null,
+    cron_expression varchar(100) not null,
+    start_date DATE not null,
+    primary key (id)
+    );
