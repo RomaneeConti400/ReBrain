5d33e1cf542bd5456b86562877ac20fbf6d64c82
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-08-30 13:45:38 +0300

    feat: created TestPostResultDto

---
+++ src/main/java/com/example/rebrain/dto/TestPostResultDto.java
@@ -0,0 +1,13 @@
+package com.example.rebrain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestPostResultDto {
+    private Long testId;
+    private Long setId;
+}
